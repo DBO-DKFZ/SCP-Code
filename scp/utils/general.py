2e3026a7d82032354f6c93e0696db62a189f4f3b
maronr <maronr@tcd.ie>
2021-11-23 17:56:05 +0100

    fix yaml path

--- scp/utils/general.py
+++ scp/utils/general.py
@@ -18,13 +18,14 @@ from os.path import basename
 
 
 # Cell
-def load_config_yaml(path_to_yaml):
+def load_config_yaml(path_to_yaml, parent_dir=None):
     '''Load project config yaml and change relative paths to absolute paths'''
 
     with open(path_to_yaml) as file:
         config = yaml.load(file, Loader=yaml.FullLoader)
 
-    parent_dir = Path(os.path.dirname(os.getcwd()))
+    if not parent_dir:
+        parent_dir = Path(os.path.dirname(os.getcwd()))
     for k, v in config.items():
         if "path" in k:
             config[k] = parent_dir/v
